fc5f31adec31bca786ea21bf3c327224783b587f
Leonardo-2112 <afoncoleonardo21@gmail.com>
2026-04-09 14:55:36 -0300

    Validando conexão com banco de dados

--- servicehub/config/conexao.php
+++ servicehub/config/conexao.php
@@ -6,9 +6,12 @@ $user = "root";
 $pass = "P@ssw0rd";
 //php.net
 try{
-    $pdo = new PDO("mysql:host=$host,dbname=$db;charset=utf8,$user,$pass");
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8",$user,$pass);
     $pdo -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    //echo "conectado com sucesso! <br>";
+    //var_dump($pdo);
 }catch(PDOException $e){
+    //var_dump($e -> getMessage());
     die("Erro na conexão: ".$e ->getMessage());
 }
 ?>
