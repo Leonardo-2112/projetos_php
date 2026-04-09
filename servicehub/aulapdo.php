ee48abeb98bddf2469e3c70525951a687ebff0f3
Leonardo-2112 <afoncoleonardo21@gmail.com>
2026-04-09 15:38:41 -0300

    Adicionando comando sql em aulapdo

--- servicehub/aulapdo.php
+++ servicehub/aulapdo.php
@@ -1,4 +1,21 @@
 <?php
 include_once "config/conexao.php";
+$sql = "select * from servicos";
+$cmd = $pdo->prepare($sql);
+$cmd -> execute();
 
+$servicos = $cmd -> fetchAll(PDO::FETCH_ASSOC);
+var_dump($servicos);
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Aula PDO</title>
+</head>
+<body>
+    <h2>Lista de Serviços</h2>
+    <table></table>
+</body>
+</html>
