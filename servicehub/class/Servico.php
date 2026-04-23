918085cc9d668b88965ab70c2ba546c6f97be232
Leonardo-2112 <afoncoleonardo21@gmail.com>
2026-04-23 17:22:48 -0300

    Adicionando a classe Servico e seus testes

--- servicehub/class/Servico.php
+++ servicehub/class/Servico.php
@@ -9,7 +9,7 @@ class Servico
     private $nome;
     private $descricao;
     private $preco;
-    private $decontinuado;
+    private $descontinuado;
     private $pdo;
     //contrutor
     public function __construct()
@@ -33,10 +33,107 @@ class Servico
         $this->nome = $nome;
     }
     //DESCRIÇÃO
-    public function getDescricao(){
+    public function getDescricao()
+    {
         return $this->descricao;
     }
-    public
+    public function setDescricao(string $descricao)
+    {
+        return $this->descricao = $descricao;
+    }
     //PREÇO
+    public function getPreco()
+    {
+        return $this->preco;
+    }
+    public function setPreco(float $preco)
+    {
+        return $this->preco = $preco;
+    }
     //DESCONTINUADO
+    public function getDescontinuado()
+    {
+        return $this->descontinuado;
+    }
+    public function setDescontinuado(bool $descontinuado)
+    {
+        return $this->descontinuado = $descontinuado;
+    }
+
+    //Métodos Obrigatórios
+    //Inserir
+    public function inserir(): bool
+    {
+        $sql = "INSERT into servicos (nome, descricao, preco, descontinuado) values(:nome, :descricao, :preco, :descontinuado)";
+        $cmd = $this->pdo->prepare($sql);
+        $cmd->bindValue(":nome", $this->nome);
+        $cmd->bindValue(":descricao", $this->descricao);
+        $cmd->bindValue(":preco", $this->preco);
+        $cmd->bindValue(":descontinuado", $this->descontinuado);
+        if ($cmd->execute()) {
+            $this->id = $this->pdo->lastInsertId();;
+            return true;
+        }
+        return false;
+    }
+    //Buscar por ID
+    public function buscarPorId(int $id): bool
+    {
+        $sql = "SELECT * FROM servicos WHERE id = :id";
+        $cmd = obterPdo()->prepare($sql);
+        $cmd->bindValue(":id", $id);
+        $cmd->execute();
+        if ($cmd->rowCount() > 0) {
+            $dados = $cmd->fetch(PDO::FETCH_ASSOC);
+            $this->id = $dados['id'];
+            $this->setNome($dados['nome']);
+            $this->setDescricao($dados['descricao']);
+            $this->setPreco($dados['preco']);
+            $this->setDescontinuado($dados['descontinuado']);
+            return true;
+        }
+        return false;
+    }
+    //Atualizar
+    public function atualizar(): bool
+    {
+        if (!$this->id) return false;
+        $sql = "UPDATE servicos SET nome = :nome, descricao = :descricao, preco = :preco, descontinuado = :descontinuado WHERE id = :id";
+        $cmd = $this->pdo->prepare($sql);
+        $cmd->bindValue(":id", $this->id);
+        $cmd->bindValue(":nome", $this->nome);
+        $cmd->bindValue(":descricao", $this->descricao);
+        $cmd->bindValue(":preco", $this->preco);
+        $cmd->bindValue(":descontinuado", $this->descontinuado);
+        return $cmd->execute();
+    }
+
+    //Listar
+    public static function listar(): array
+    {
+        $sql = "SELECT * FROM servicos";
+        $cmd = obterPdo()->prepare($sql);
+        $cmd->execute();
+        return $cmd->fetchAll();
+    }
+    //Listar Ativos
+    public static function listarAtivos(): array
+    {
+        $sql = "SELECT * FROM servicos WHERE descontinuados = 0";
+        $cmd = obterPdo()->prepare($sql);
+        $cmd->execute();
+        return $cmd->fetchAll();
+    }
+    //Excluir
+        public function excluir():bool{
+        if(!$this->id )return false;
+        $sql = "UPDATE servicos SET descontinuado = :descontinuado WHERE id = :id";
+        $cmd = $this->pdo->prepare($sql);
+        $cmd->bindValue(":id",$this->id);
+        $cmd->bindValue(":nome",$this->nome);
+        $cmd->bindValue(":descricao",$this->descricao);
+        $cmd->bindValue(":preco",$this->preco);
+        $cmd->bindValue(":descontinuado",$this->descontinuado, PDO::PARAM_BOOL);
+        return $cmd->execute();
+        }
 }
---
+++ servicehub/testeServico.php
@@ -0,0 +1,59 @@
+<?php
+require_once "class/Servico.php";
+ini_set('display_errors',1);
+ini_set('display_startup_erros', 1);
+error_reporting(E_ALL);
+//Teste Inserir
+$servico = new Servico();
+$servico->setNome('Limpeza de Piscina');
+$servico->setDescricao('limpeza de piscina completa.');
+$servico->setPreco(200);
+$servico->setDescontinuado(0);
+if ($servico->inserir()){
+    echo "Servico com ID: ".$servico->getId()."<br>Nome: ".$servico->getNome()."<br>Descrição: ".$servico->getDescricao(). "<br>Preço: " . $servico->getPreco() . "<br>Inserido Com Sucesso";
+}
+
+//Teste Buscar Por Id
+ $servico = new Servico();
+ if ($servico->buscarPorId(11)){
+     echo "<pre>";
+     echo "Id: ".$servico->getId()."<br>Nome: ".$servico->getNome()."<br>Descrição: ".$servico->getDescricao(). "<br>Preço: " . $servico->getPreco(). "<br>Descontinuado: ".$servico->getDescontinuado();
+ }else{
+     echo "Cliente não Cadastrado";
+     die();
+ }
+
+//Teste Atualizar
+$servico = new Servico();
+$servico->buscarPorId(36);
+$servico->setNome("Limpeza de Piscina Premium");
+$servico->setDescricao("Limpeza completa da piscina, incluindo remoção de folhas e detritos,
+escovação das paredes e aspiração do fundo, além de tratamento
+químico para manter a água cristalina por mais tempo.");
+$servico->setPreco(200.00);
+if ($servico->atualizar()) {
+    print_r($servico);
+    echo "Serviço atualizado com sucesso!";
+}
+
+//Teste Listar
+$servicos = Servico::listar();
+foreach ($servicos as $servico) {
+    echo "Id: ".$servico->getId()."<br>Nome: ".$servico->getNome()."<br>Descrição: ".$servico->getDescricao(). "<br>Preço: " . $servico->getPreco(). "<br>Descontinuado: ".$servico->getDescontinuado();
+}
+
+//Teste Listar Ativos
+$servicos = Servico::listarAtivos();
+foreach ($servicos as $servico) {
+    echo "Id: ".$servico->getId()."<br>Nome: ".$servico->getNome()."<br>Descrição: ".$servico->getDescricao(). "<br>Preço: " . $servico->getPreco(). "<br>Descontinuado: ".$servico->getDescontinuado();
+}
+
+
+//Teste Excluir
+$servico = new Servico();
+$servico->buscarPorId(36);
+if ($servico->excluir()) {
+    echo "Serviço excluído com sucesso!";
+} else {
+    echo "Erro ao excluir serviço.";
+}
